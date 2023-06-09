545cac136300b0cd16086e136ef34b46fed15a82
GabrielOstrowski <131359366+GabrielOstrowski@users.noreply.github.com>
2023-06-09 13:59:31 +0200

    Update exec.py

--- exec.py
+++ exec.py
@@ -1,1 +1,7 @@
-
+pyinstaller.exe --onefile task1.py
+pyinstaller.exe --onefile task2.py
+pyinstaller.exe --onefile task3.py
+pyinstaller.exe --onefile task4.py
+pyinstaller.exe --onefile task5.py
+pyinstaller.exe --onefile task6.py
+pyinstaller.exe --onefile task7.py
